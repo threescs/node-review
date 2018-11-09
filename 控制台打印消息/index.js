53780fe2ac812de6ad89abf9f3c7f1ea1260935a
尚成帅 <95513113@qq.com>
2018-11-09 16:50:14 +0800

    控制台打印消息

--- 控制台打印消息/index.js
+++ 控制台打印消息/index.js
@@ -5,4 +5,4 @@ signale.debug('Hello', 'from', 'L59');
 signale.pending('Write release notes for 1.2.0');
 signale.fatal(new Error('Unable to acquire lock'));
 signale.watch('Recursively watching build directory...');
-signale.complete({prefix: '[task]', message: 'Fix issue #59', suffix: '(@klauscfhq)'});
+signale.complete({prefix: '[task]', message: 'Fix issue #59', suffix: '(@klauscfhq)'})
